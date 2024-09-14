f718838f33d94eb1193bd445225a6100609069e7
Yesid Marin <yesidmarin05@gmail.com>
2024-09-13 19:04:11 -0500

    version 1.0.7

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["AKFaceTecSwiftWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Autentikar/faceTec-dependency.git", from: "9.7.29"),
+        .package(url: "https://github.com/Autentikar/faceTec-dependency.git", from: "9.7.30"),
     ],
     targets: [
         .target(
